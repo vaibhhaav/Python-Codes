dc93f68502d68bcccbcab224433a0d68f6c85211
vaibhhaav <vaibhavgupta2021@gmail.com>
2024-08-23 10:13:05 +0530

    Various codes

--- TracetheProgram_AreaofCircle.py
+++ TracetheProgram_AreaofCircle.py
@@ -4,3 +4,4 @@ area = radius * radius * 3.14
 
 #Display results
 print("The area of the circle of radius " + str(radius) + " is " + str(area))
+# print("The area of the circle of radius ", radius," is ", area) Or this could be the Alternative
