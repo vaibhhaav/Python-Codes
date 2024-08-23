ae566ecde3aa9a419276ba335735aaeabfe089aa
vaibhhaav <vaibhavgupta2021@gmail.com>
2024-08-23 11:35:07 +0530

    Various Codes

--- Expression.py
+++ Expression.py
@@ -1,7 +1,9 @@
 x = 1 # Assign 1 to variable x
 radius = 1.0 # Assign 1.0 to variable radius
-
+y = 1
 # Assign the value of the expression to x
 x = 5 * (3/2) + 3 * 2
 x = y + 1 # Assign the addition of y and 1 to x
 area = radius * radius * 3.14 # Compute area
+print(area)
+print("x =", x)
