770c18b666c6c194a11a2cd8777e0f8de557d551
vaibhhaav <vaibhavgupta2021@gmail.com>
2024-08-25 10:30:32 +0530

    Various codes

--- Fibonacci_serie.py
+++ Fibonacci_serie.py
@@ -2,9 +2,9 @@ a=0
 b=1
 n=eval(input("Enter number of terms: "))
 print("Fibonacci Series")
-print(a,b)
+print(a,b,end =' ')
 for i in range(1,n,1):
     c=a+b
-    print(c)
+    print(c,end=' ')
     a=b
     b=c
